d16877f0a16b1fed1abd4abc3b0c520aabfa0f16
zemosamurai <zemosamurai@gmail.com>
2023-05-12 22:39:35 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src"
+    }
+  },
+  base: '/fake-vk',
+  server: {
+    port: 3000,
+    host: true
+  }
+})
