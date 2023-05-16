710c8a63fe196824185b0110481474c8be749e46
zemosamurai <zemosamurai@gmail.com>
2023-05-16 23:08:47 +0300

    add: font Roboto

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: "/src"
-    }
-  },
-  base: '/fake-vk',
-  server: {
-    port: 3000,
-    host: true
-  }
+	plugins: [svgr(), react()],
+	resolve: {
+		alias: {
+			src: '/src',
+		},
+	},
+	base: '/fake-vk',
+	server: {
+		port: 3000,
+		host: true,
+	},
 })
